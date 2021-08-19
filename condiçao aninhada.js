946b7d59ea22675917d32057b52c3a3a29b5c404
Gustavo pereira da silva <79516858+gustavo-pereira-da-silva@users.noreply.github.com>
2021-08-18 22:28:12 -0300

    Add files via upload

---
+++ condiçao aninhada.js
@@ -0,0 +1,9 @@
+var idade = 67
+console.log(`Voce tem Exatamente ${idade} anos`)
+if  (idade  < 16 ){
+    console.log('Nao vota')
+}else if (idade < 18 || idade>65){
+console.log('Voto Opcional')
+}else{
+    console.log ('Voto Obrigatorio')
+}
