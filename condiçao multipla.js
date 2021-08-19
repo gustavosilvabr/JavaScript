c13713a1659787aa79c0c561d38e8640d596fdce
Gustavo pereira da silva <79516858+gustavo-pereira-da-silva@users.noreply.github.com>
2021-08-18 23:33:43 -0300

    Add files via upload

---
+++ condiçao multipla.js
@@ -0,0 +1,27 @@
+var diasem= new Date()
+var hoje = diasem.getDay()
+
+
+switch(hoje){
+    case 0:
+        console.log('Domingo')
+        break
+        case 1 : 
+        console.log('Segunda')
+        break
+        case 2 : 
+        console.log('Terça')
+        break
+        case 3 : 
+        console.log('Quarta')
+        break
+        case 4 : 
+        console.log('Quinta')
+        break
+        case 5 :
+            console.log('Sexta') 
+            break
+        case 6 : 
+        console.log('Sabado')
+
+}
